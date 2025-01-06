a5a9c63ce7d8013eaa771c6f57ae23e9b9abb476
lbatschelet <batschelet.lukas@gmail.com>
2025-01-06 15:18:32 +0100

    finally working homescreen

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,22 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname);
+const config = (() => {
+  const baseConfig = getDefaultConfig(__dirname);
+
+  const { transformer, resolver } = baseConfig;
+
+  baseConfig.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
+  };
+  baseConfig.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"]
+  };
+
+  return baseConfig;
+})();
 
 module.exports = withNativeWind(config, { input: './global.css' });
